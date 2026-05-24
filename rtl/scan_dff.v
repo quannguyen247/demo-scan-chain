849d78cf06ea45d7bd5980e947eedf4804d974ca
quannguyen247 <nguyendongquan247@gmail.com>
2026-05-24 17:15:39 +0700

    Fix scan_dff.v

--- rtl/scan_dff.v
+++ rtl/scan_dff.v
@@ -1,54 +1,24 @@
 `timescale 1ns/1ps
 
-module counter_scan (
+module scan_dff (
     input clk,
     input rst,
-    input en,
     input scan_en,
+    input d,
     input scan_in,
-    output scan_out,
-    output [3:0] q
+    output reg q
 );
 
-    wire [3:0] next_q;
-    assign next_q = en ? (q + 1'b1) : q;
-
-    scan_dff u_scan_dff0 (
-        .clk(clk),
-        .rst(rst),
-        .scan_en(scan_en),
-        .d(next_q[0]),
-        .scan_in(scan_in),
-        .q(q[0])
-    );
-
-    scan_dff u_scan_dff1 (
-        .clk(clk),
-        .rst(rst),
-        .scan_en(scan_en),
-        .d(next_q[1]),
-        .scan_in(q[0]),
-        .q(q[1])
-    );
-
-    scan_dff u_scan_dff2 (
-        .clk(clk),
-        .rst(rst),
-        .scan_en(scan_en),
-        .d(next_q[2]),
-        .scan_in(q[1]),
-        .q(q[2])
-    );
-
-    scan_dff u_scan_dff3 (
-        .clk(clk),
-        .rst(rst),
-        .scan_en(scan_en),
-        .d(next_q[3]),
-        .scan_in(q[2]),
-        .q(q[3])
-    );
-   
-    assign scan_out = q[3];
+    always @(posedge clk) begin
+        if (rst) begin
+            q <= 1'b0;
+        end
+        else if (scan_en) begin
+            q <= scan_in;
+        end
+        else begin
+            q <= d;
+        end
+    end
 
 endmodule
